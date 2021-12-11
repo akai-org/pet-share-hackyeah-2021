fcc75fcee863411b937a193fac3908810e587aba
mozzar <realmozzar@gmail.com>
2021-12-11 17:57:14 +0100

    update modeli

--- petapp/views.py
+++ petapp/views.py
@@ -1,7 +1,10 @@
+import json
+import profile
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from petapp.models import ItemType
+from petapp.models import *
 
 
 @api_view(http_method_names=['GET'])
