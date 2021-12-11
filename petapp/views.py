73ee3baf7bcef7076a93b77e5b551e10c13d4bd1
Radosław Winiecki <r.winiecki@outlook.com>
2021-12-11 17:58:05 +0100

    some profile tests, idk

--- petapp/views.py
+++ petapp/views.py
@@ -1,9 +1,25 @@
+from django.contrib.auth.models import User
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from petapp.models import ItemType
+from petapp.models import ItemType, Profile
 
 
 @api_view(http_method_names=['GET'])
 def get_all_item_types(request):
     return Response(ItemType.objects.all().values())
+
+
+@api_view(http_method_names=['GET', 'POST'])
+def users(request):
+    if request.method == 'GET':
+        return Response(Profile.objects.all().values())
+    else:
+        profile = request.data
+        print(profile['user'])
+        user = User(**(profile['user']))
+        if len(User.objects.filter(id=user.id).values()):
+            user.save()
+        profile['user'] = user
+        Profile(**profile).save()
+        return Response(Profile.objects.all().values())
